353c1b092ad676bc225e47f9b793fb246bd96f6b
Rasmus Bak Christiansen <rasmus.chr11@gmail.com>
2019-02-09 04:53:40 +0100

    Opd

--- MediaServer/MusicRecoder.cs
+++ MediaServer/MusicRecoder.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using NAudio.Wave;
 using System.IO;
 
@@ -13,14 +14,19 @@ namespace MediaServer
 
         private WaveFormatConversionStream audioStream;
 
-        private byte[][] audioBuffers = new byte[2][];
+        private byte[][] audioBuffers = new byte[3][];
         public PlaybackState state = PlaybackState.Stopped;
         private int bufferInUse = 0;
-        public int bufferSize = 1024*256;
+        private int bufferSize = 1024;
+
 
         Task currentBufferWriter;
 
-        public int BufferSize { get { return bufferSize; } }
+        public delegate byte[] PlayDel();
+
+        public PlayDel Play; 
+
+        public int BufferSize { get { return bufferSize; } set { bufferSize = value; } }
 
         public WaveFormat WaveFormat { get { return audioStream.WaveFormat; } }
 
@@ -30,68 +36,74 @@ namespace MediaServer
             audioBuffers[1] = new byte[bufferSize];
 
             string extension = Path.GetExtension(filePath);
+            Play = PlayFileWave;
             switch (extension)
             {
-                case ".mp3": Mp3FileReader mp3 = new Mp3FileReader(filePath); 
-                        audioStream = new WaveFormatConversionStream(mp3.WaveFormat, mp3);
+                case ".mp3": Mp3FileReader mp3 = new Mp3FileReader(filePath);
+                    audioStream = new WaveFormatConversionStream(mp3.WaveFormat, mp3);
                     break;
                 case ".wma": MediaFoundationReader wma = new MediaFoundationReader(filePath);
-                        audioStream = new WaveFormatConversionStream(wma.WaveFormat, wma);
+                    audioStream = new WaveFormatConversionStream(wma.WaveFormat, wma);
                     break;
                 case ".m4a": MediaFoundationReader m4a = new MediaFoundationReader(filePath);
                     audioStream = new WaveFormatConversionStream(m4a.WaveFormat, m4a); ;
                     break;
+                default: Play = PlaySystemInput;
+                    break;
             }
         }
-        
-        public byte[] Play()
+        public byte[] PlaySystemInput()
         {
-            
-                int prevBuffer = bufferInUse;
-                long pos = audioStream.Position;
-                if(currentBufferWriter != null)
-                    currentBufferWriter.Wait();
-                int remain = Convert.ToInt32(audioStream.Length - audioStream.Position);
-                Console.WriteLine(remain + "<" + bufferSize);
-                if (remain < bufferSize)
-                {
-                    state = PlaybackState.Stopped;
-                    if (remain != 0)
-                    {
-                        int newBufferSize = bufferSize + remain;
-                        byte[] tempB = new byte[newBufferSize];
-                        Array.Copy(audioBuffers[bufferInUse], tempB, bufferSize);
-                        audioStream.Read(tempB, bufferSize, newBufferSize);
-
-                        bufferSize = newBufferSize;
-                        return tempB;
-                    }
-                    else
-                        return audioBuffers[bufferInUse];
-                }
-                if (state == PlaybackState.Paused || state == PlaybackState.Stopped)
-                {
-                    state = PlaybackState.Playing;
-                    audioStream.Read(audioBuffers[bufferInUse], 0, bufferSize);
-                    switchBuffer();
-                    currentBufferWriter = audioStream.ReadAsync(audioBuffers[bufferInUse], 0, bufferSize);
-                }
-                else
-                {
-                    switchBuffer();
-                    currentBufferWriter = audioStream.ReadAsync(audioBuffers[bufferInUse], 0, bufferSize);
-                }
-                return audioBuffers[prevBuffer];
-
-                //switchBuffer();
-                //int remain = Convert.ToInt32(audioStream.Length - audioStream.Position);
-                //audioStream.Read(audioBuffers[bufferInUse], 0, remain);
-                //return audioBuffers[bufferInUse];
-         
-            
-               
+            return new byte[0];
         }
 
+            public byte[] PlayFileWave()
+        {
+
+            int prevBuffer = bufferInUse;
+            long pos = audioStream.Position;
+            if (currentBufferWriter != null)
+                currentBufferWriter.Wait();
+            int remain = Convert.ToInt32(audioStream.Length - audioStream.Position);
+            Console.WriteLine(remain + "<" + bufferSize);
+            if (remain < bufferSize)
+            {
+                state = PlaybackState.Stopped;
+                if (remain != 0)
+                {
+                    int newBufferSize = bufferSize + remain;
+                    byte[] tempB = new byte[newBufferSize];
+                    Array.Copy(audioBuffers[bufferInUse], tempB, bufferSize);
+                    audioStream.Read(tempB, bufferSize, newBufferSize);
+
+                    bufferSize = newBufferSize;
+                    return tempB;
+                }
+                else
+                    return audioBuffers[bufferInUse];
+            }
+            if (state == PlaybackState.Paused || state == PlaybackState.Stopped)
+            {
+                state = PlaybackState.Playing;
+                audioStream.Read(audioBuffers[bufferInUse], 0, bufferSize);
+                switchBuffer();
+                currentBufferWriter = audioStream.ReadAsync(audioBuffers[bufferInUse], 0, bufferSize);
+            }
+            else
+            {
+                switchBuffer();
+                currentBufferWriter = audioStream.ReadAsync(audioBuffers[bufferInUse], 0, bufferSize);
+            }
+            return audioBuffers[prevBuffer];
+
+            //switchBuffer();
+            //int remain = Convert.ToInt32(audioStream.Length - audioStream.Position);
+            //audioStream.Read(audioBuffers[bufferInUse], 0, remain);
+            //return audioBuffers[bufferInUse];
+
+
+
+        }
 
         private void switchBuffer()
         {
@@ -103,12 +115,11 @@ namespace MediaServer
                 bufferInUse = 0;
         }
 
-        public void clearBuffers() 
+        public void clearBuffers()
         {
-            
             audioBuffers[0] = new byte[bufferSize];
             audioBuffers[1] = new byte[bufferSize];
         }
-        
+
     }
 }
--- MediaServer/Program.cs
+++ MediaServer/Program.cs
@@ -6,13 +6,108 @@ using System.Threading.Tasks;
 using NAudio.Wave;
 using System.Threading;
 using System.IO;
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
+using Microsoft.CSharp;
+using System.Net.Sockets;
+using System.Web;
+using System.Net;
 
 namespace MediaServer
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            MMDeviceEnumerator mmDeviceEnum = new MMDeviceEnumerator();
+            bool run = true;
+            Task.Factory.StartNew(() =>
+            {
+                Console.ReadLine();
+                run = false;
+            });
+            Task.Factory.StartNew(async () => 
+            {
+
+                UdpClient udpHost = new UdpClient(8080);
+                BufferedWaveProvider MDWaveProvider = new BufferedWaveProvider(new WaveFormat());
+                MDWaveProvider.DiscardOnBufferOverflow = true;
+                WaveOut windowsPlayerMic = new WaveOut();
+                Console.WriteLine("Connected");
+                windowsPlayerMic.Init(MDWaveProvider);
+                windowsPlayerMic.Play();
+                bool runClient = true;
+                TimeSpan timeCool = new TimeSpan(0, 0, 2);
+                TimeSpan? timeEnd = null;
+                DateTime? time = null;
+                while (runClient)
+                {
+                    if (udpHost.Available > 0) {
+                        Console.WriteLine($"Awaiting network, {udpHost.Available}");
+                        IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                        byte[] buffer = udpHost.Receive(ref RemoteIpEndPoint);
+                        MDWaveProvider.AddSamples(buffer, 0, buffer.Length);
+                        Console.WriteLine($"Recieved bytes {buffer.Length} from network");
+                    }
+                    else
+                    {
+
+                    }
+                    if(!run)
+                    {
+                        if (time == null)
+                            time = DateTime.Now;
+                        timeEnd = (DateTime.Now - time);
+                        runClient = (timeEnd ?? new TimeSpan(0,0,5)) < timeCool;
+                    }
+                }
+                Thread.Sleep(55);
+                Console.WriteLine($"stopped client after {(timeEnd ?? new TimeSpan(1, 0, 0)).TotalSeconds} seconds");
+            });
+
+            UdpClient udpClient = new UdpClient();
+            udpClient.Connect("localhost", 8080);
+
+            foreach (MMDevice d in mmDeviceEnum.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active))
+            {
+                Console.WriteLine(d.DeviceFriendlyName);
+
+                WasapiCapture ws = new WasapiCapture(d);
+                ws.WaveFormat = new WaveFormat();
+                ws.ShareMode = AudioClientShareMode.Exclusive;
+                try
+                {
+                    ws.DataAvailable += (s, a) =>
+                    {
+                        udpClient.SendAsync(a.Buffer, a.BytesRecorded);
+                        Console.WriteLine($"Send bytes {a.BytesRecorded} to network");
+                    };
+                    Console.WriteLine(ws.CaptureState);
+                    ws.StartRecording();
+                    while ((ws.CaptureState != NAudio.CoreAudioApi.CaptureState.Stopped || udpClient.Client.Connected) && run)
+                    {
+                        Thread.Sleep(500);
+                    }
+                }
+                finally { ws.Dispose(); d.Dispose();
+                }
+                TimeSpan timeCool = new TimeSpan(0, 0, 2);
+                TimeSpan? timeEnd = null;
+                DateTime? time = null;
+                bool runHost = true;
+                while (runHost)
+                {
+                    if (time == null)
+                        time = DateTime.Now;
+                    timeEnd = (DateTime.Now - time);
+                    runHost = (timeEnd ?? new TimeSpan(0, 0, 5)) < timeCool;
+                }
+                udpClient.Close();
+                Console.WriteLine($"stopped host after {(timeEnd ?? new TimeSpan(1, 0, 0)).TotalSeconds} seconds");
+            }
+            Console.ReadLine();
+            /*
             WaveOutEvent woe = new WaveOutEvent();
 
             woe.NumberOfBuffers = 2;
@@ -36,8 +131,9 @@ namespace MediaServer
                 WaveOut windowsPlayer = new WaveOut();
 
                 BufferedWaveProvider provider = new BufferedWaveProvider(mr.WaveFormat);
+                provider.BufferLength = 1024 * 512;
                 double temp = provider.BufferLength / 3;
-                mr.bufferSize = Convert.ToInt32(Math.Ceiling(temp));
+                mr.BufferSize = Convert.ToInt32(Math.Ceiling(temp));
                 mr.clearBuffers();
 
                 windowsPlayer.Init(provider);
@@ -86,7 +182,7 @@ namespace MediaServer
                 windowsPlayer.Stop();
                 Thread.Sleep(100);
             }
-
+            */
 
         }
     }
